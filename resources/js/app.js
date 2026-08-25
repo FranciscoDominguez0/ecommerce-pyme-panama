0f9ea706f4a12342ec915464180e2c838963d5eb
Francisco <dominguezf225@gmail.com>
2026-08-25 04:30:31 -0500

    borrar libreria websocket

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,5 @@ if (!window.Livewire && !document.querySelector('[wire\\:snapshot]')) {
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
  */
 
-import './echo';
